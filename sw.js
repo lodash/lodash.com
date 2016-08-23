b90c4126749771214597a9e494a12e6b39068ffa
John-David Dalton <john.david.dalton@gmail.com>
2016-08-22 21:45:09 -0700

    Use cache bust query instead of no-cache.

--- sw.js
+++ sw.js
@@ -24,44 +24,41 @@ var prefetch = [
   'https://npmcdn.com/react-dom@{{ site.react.version }}/dist/react-dom.min.js'
 ];
 
-/**
- * Checks is a response status is classified as
- * [OK](https://fetch.spec.whatwg.org/#ok-status).
- *
- * @private
- * @param {number} status The status code to check.
- * @returns {boolean} Returns `true` if `status` is OK, else `false`.
- */
-function isOk(status) {
-  return !status || (status >= 200 && status <= 299);
-}
-
 /*----------------------------------------------------------------------------*/
 
-addEventListener('install', event =>
+addEventListener('install', event => {
+  const now = Date.now();
   event.waitUntil(Promise.all([
     skipWaiting(),
     caches.open(CACHE_KEY).then(cache =>
-      Promise.all(prefetch.map(entry =>
+      Promise.all(prefetch.map(uri => {
+        // Use cache-bust query until cache modes are supported in Chrome.
+        // Only add to same-origin requests to avoid potential 403 responses.
+        // See https://github.com/mjackson/npm-http-server/issues/44.
+        const url = new URL(uri, location.href);
+        const input = url.origin == location.origin
+          ? (url.search += `${ url.search ? '&' : '?' }cache-bust=${ now }`, url.href)
+          : uri;
+
         // Attempt to prefetch and cache with 'cors'.
-        fetch(entry, { 'cache': 'no-cache', 'mode': 'cors' })
-          .then(response => isOk(response.status) && cache.put(entry, response))
+        return fetch(input)
+          .then(response => response.ok && cache.put(uri, response))
           .catch(() =>
             // Fallback to prefetch and cache with 'no-cors'.
-            fetch(entry, { 'cache': 'no-cache', 'mode': 'no-cors' })
+            fetch(input, { 'mode': 'no-cors' })
               .then(response => {
-                if (!isOk(response.status)) {
+                if (response.status && !response.ok) {
                   throw new TypeError('Response status is !ok');
                 }
-                cache.put(entry, response);
+                cache.put(uri, response);
               })
               // Prefetch failed.
-              .catch(event => console.log(`prefetch failed: ${ entry }`, error))
-          )
-      ))
+              .catch(event => console.log(`prefetch failed: ${ uri }`, error))
+          );
+      }))
     )
   ]))
-);
+});
 
 addEventListener('activate', event =>
   event.waitUntil(Promise.all([
