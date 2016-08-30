0d8dc58293afa39e4db680e08354827f9a838023
John-David Dalton <john.david.dalton@gmail.com>
2016-08-29 22:22:22 -0700

    Loose equality nit.

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -249,7 +249,7 @@
   versionSelect.addEventListener('change', function(event) {
     var value = event.target.value;
     if (value) {
-      location.href = value === '1.3.1'
+      location.href = value == '1.3.1'
         ? 'https://github.com/lodash/lodash/blob/1.3.1/doc/README.md'
         : '/docs/' + value;
     }
