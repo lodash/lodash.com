715fe9e799aca59dbb04f7714e917f331516aff3
John-David Dalton <john.david.dalton@gmail.com>
2016-09-11 15:26:20 -0700

    Cleanup tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -20,19 +20,29 @@ const cb = e => e && console.log(e.message);
 const opts = { base };
 const manifest = require('./manifest.json');
 
+/*----------------------------------------------------------------------------*/
+
 gulp.task('build-css', () =>
   pump([
-    gulp.src('_site/assets/css/*.css', opts),
-    purify(['_site/assets/js/*.js', './_site/**/*.html'], { 'rejected': true }),
+    gulp.src('_site/**/*.css', opts),
+    purify(['_site/**/*.html', '_site/assets/**/*.js'], { 'rejected': true }),
     cssnano(),
     gulp.dest(base)
   ])
 );
 
-gulp.task('build-svg', () =>
+gulp.task('build-html', () =>
   pump([
-    gulp.src('_site/assets/img/*.svg', opts),
-    imagemin(),
+    gulp.src('_site/**/*.html', opts),
+    htmlmin({
+      'collapseBooleanAttributes': true,
+      'collapseWhitespace': true,
+      'removeAttributeQuotes': true,
+      'removeComments': true,
+      'removeEmptyAttributes': true,
+      'removeOptionalTags': true,
+      'removeRedundantAttributes': true
+    }),
     gulp.dest(base)
   ], cb)
 );
@@ -68,20 +78,15 @@ gulp.task('build-js', () =>
   ], cb)
 );
 
-gulp.task('build-html', () =>
+gulp.task('build-svg', () =>
   pump([
-    gulp.src('_site/**/*.html', opts),
-    htmlmin({
-      'collapseBooleanAttributes': true,
-      'collapseWhitespace': true,
-      'removeAttributeQuotes': true,
-      'removeComments': true,
-      'removeEmptyAttributes': true,
-      'removeOptionalTags': true,
-      'removeRedundantAttributes': true
-    }),
+    gulp.src('_site/**/*.svg', opts),
+    imagemin(),
     gulp.dest(base)
   ], cb)
 );
 
-gulp.task('build', sequence(['build-css', 'build-js', 'build-html'], 'build-svg', 'build-icon'));
+gulp.task('build', sequence(
+  ['build-css', 'build-html', 'build-js', 'build-svg'],
+  'build-icon'
+));
