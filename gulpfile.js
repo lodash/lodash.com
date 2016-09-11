c870db51c9a58f5ad2c0c762d2b364442f2af463
John-David Dalton <john.david.dalton@gmail.com>
2016-09-11 15:00:22 -0700

    Add manifest icon generation.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 'use strict';
 
+const _ = require('lodash');
+const path = require('path');
+
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const pump = require('pump');
 
 const cssnano = require('gulp-cssnano');
 const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
 const purify = require('gulp-purifycss');
+const responsive = require('gulp-responsive');
+const sequence = require('gulp-sequence');
 const uglify = require('gulp-uglify');
 
 const base = './';
+const cb = e => e && console.log(e.message);
 const opts = { base };
+const manifest = require('./manifest.json');
 
 gulp.task('build-css', () =>
   pump([
@@ -21,13 +29,43 @@ gulp.task('build-css', () =>
   ])
 );
 
+gulp.task('build-svg', () =>
+  pump([
+    gulp.src('_site/assets/img/*.svg', opts),
+    imagemin(),
+    gulp.dest(base)
+  ], cb)
+);
+
+gulp.task('build-icon', () =>
+  pump([
+    gulp.src('_site/assets/img/lodash.svg'),
+    responsive({
+      'lodash.svg': _(manifest.icons)
+        .reject(['sizes', 'any'])
+        .map(icon => {
+          const sizes = icon.sizes.split('x');
+          return {
+            'flatten': true,
+            'height': sizes[1],
+            'rename': path.basename(icon.src),
+            'width': sizes[0]
+          };
+        })
+        .value()
+    }),
+    imagemin(),
+    gulp.dest('_site/icon/')
+  ], cb)
+);
+
 gulp.task('build-js', () =>
   pump([
     gulp.src('_site/**/*.js', opts),
     babel({ 'presets': ['es2015'] }),
     uglify(),
     gulp.dest(base)
-  ])
+  ], cb)
 );
 
 gulp.task('build-html', () =>
@@ -43,7 +81,7 @@ gulp.task('build-html', () =>
       'removeRedundantAttributes': true
     }),
     gulp.dest(base)
-  ])
+  ], cb)
 );
 
-gulp.task('build', ['build-css', 'build-js', 'build-html']);
+gulp.task('build', sequence(['build-css', 'build-js', 'build-html'], 'build-svg', 'build-icon'));
