88fa13ece5fd6401c257a744756232abc0d2f3a4
John-David Dalton <john.david.dalton@gmail.com>
2016-09-12 10:47:17 -0700

    Make height/width numbers.

--- icons.js
+++ icons.js
@@ -13,8 +13,8 @@ module.exports = {
       const sizes = icon.sizes.split('x');
       return {
         'rename': path.basename(icon.src),
-        'width': sizes[0],
-        'height': sizes[1],
+        'width': +sizes[0],
+        'height': +sizes[1],
         'embed': true,
         'flatten': true
       };
@@ -22,29 +22,29 @@ module.exports = {
     .push(
       {
         'rename': 'mstile-150x150.png',
-        'width': '150',
-        'height': '150',
+        'width': 150,
+        'height': 150,
         'embed': true,
         'flatten': true
       },
       {
         'rename': 'apple-touch-180x180.png',
-        'width': '180',
-        'height': '180',
+        'width': 180,
+        'height': 180,
         'embed': true,
         'flatten': true
       },
       {
         'rename': 'mstile-310x150.png',
-        'width': '310',
-        'height': '150',
+        'width': 310,
+        'height': 150,
         'embed': true,
         'flatten': true
       },
       {
         'rename': 'mstile-310x310.png',
-        'width': '310',
-        'height': '310',
+        'width': 310,
+        'height': 310,
         'embed': true,
         'flatten': true
       }
@@ -53,14 +53,14 @@ module.exports = {
   'icons/favicon-16x16.png': [
     {
       'rename': 'favicon-32x32.png',
-      'width': '32',
-      'height': '32',
+      'width': 32,
+      'height': 32,
       'interpolation': 'nearest'
     },
     {
       'rename': 'favicon-48x48.png',
-      'width': '48',
-      'height': '48',
+      'width': 48,
+      'height': 48,
       'interpolation': 'nearest'
     }
   ],
