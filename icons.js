98fdb43208d3f1784d6eee42e3131e2ba3354147
John-David Dalton <john.david.dalton@gmail.com>
2016-09-15 10:09:14 -0700

    Update gulp-responsive.

--- icons.js
+++ icons.js
@@ -49,19 +49,19 @@ module.exports = {
       'rename': 'favicon-16x16.png',
       'width': 16,
       'height': 16,
-      'interpolation': 'nearest'
+      'interpolator': 'nearest'
     },
     {
       'rename': 'favicon-32x32.png',
       'width': 32,
       'height': 32,
-      'interpolation': 'nearest'
+      'interpolator': 'nearest'
     },
     {
       'rename': 'favicon-48x48.png',
       'width': 48,
       'height': 48,
-      'interpolation': 'nearest'
+      'interpolator': 'nearest'
     }
   ],
 };
