1338b93c9fabd47c459872e4970f859c493c4d06
John-David Dalton <john.david.dalton@gmail.com>
2016-09-16 01:26:47 -0700

    Make mstiles have a background.

--- icons.js
+++ icons.js
@@ -14,53 +14,58 @@ module.exports = {
       return {
         'rename': path.basename(icon.src),
         'width': +sizes[0],
-        'height': +sizes[1]
+        'height': +sizes[1],
+        'background': 'rgba(0,0,0,0)'
       };
     })
     .push(
       {
-        'rename': 'mstile-150x150.png',
-        'width': 150,
-        'height': 150
-      },
-      {
         'rename': 'apple-touch-180x180.png',
         'width': 180,
-        'height': 180
+        'height': 180,
+        'background': 'rgba(0,0,0,0)'
+      },
+      {
+        'rename': 'mstile-150x150.png',
+        'width': 150,
+        'height': 150,
+        'embed': true,
+        'flatten': true
       },
       {
         'rename': 'mstile-310x150.png',
         'width': 310,
-        'height': 150
+        'height': 150,
+        'embed': true,
+        'flatten': true
       },
       {
         'rename': 'mstile-310x310.png',
         'width': 310,
-        'height': 310
+        'height': 310,
+        'embed': true,
+        'flatten': true
       }
     )
-    .map(icon => _.assign(icon, {
-      'background': 'rgba(0,0,0,0)',
-      'embed': true
-    }))
     .value(),
   'icons/favicon-16x16.png': [
     {
       'rename': 'favicon-16x16.png',
       'width': 16,
-      'height': 16
+      'height': 16,
+      'interpolator': 'nearest'
     },
     {
       'rename': 'favicon-32x32.png',
       'width': 32,
-      'height': 32
+      'height': 32,
+      'interpolator': 'nearest'
     },
     {
       'rename': 'favicon-48x48.png',
       'width': 48,
-      'height': 48
+      'height': 48,
+      'interpolator': 'nearest'
     }
-  ].map(icon => _.assign(icon, {
-    'interpolator': 'nearest'
-  }))
+  ]
 };
