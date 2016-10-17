18a882bd40369436ebd24439da48ba0ec3751786
John-David Dalton <john.david.dalton@gmail.com>
2016-10-16 22:59:24 -0700

    Add online check back to ga bootstrap.

--- assets/js/boot.ga.js
+++ assets/js/boot.ga.js
@@ -1,13 +1,14 @@
-this[GoogleAnalyticsObject = '_ga'] = {
-  'l': Date.now(),
-  'q': [
-    ['create', 'UA-6065217-64', 'auto'],
-    ['send', 'pageview']
-  ]
-};
-
-document.head.appendChild(function() {
-  var script = document.createElement('script');
-  script.src = 'https://www.google-analytics.com/analytics.js';
-  return script;
-}());
+if (navigator.onLine) {
+  this[GoogleAnalyticsObject = '_ga'] = {
+    'l': Date.now(),
+    'q': [
+      ['create', 'UA-6065217-64', 'auto'],
+      ['send', 'pageview']
+    ]
+  };
+  document.head.appendChild(function() {
+    var script = document.createElement('script');
+    script.src = 'https://www.google-analytics.com/analytics.js';
+    return script;
+  }());
+}
