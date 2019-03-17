890b364448e7076aae6421f0bf2a8bd8dbb76f74
Jean-Philippe Sirois <jp@veksen.net>
2019-03-17 19:54:27 -0400

    ui: wip on hero content

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-plugin-react-svg`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,11 +1,17 @@
 import React from "react"
 import styled from "styled-components"
+import Logo from "../images/lodash.svg"
 import Container from "./container"
 
 const HeroWrapper = styled.header`
   display: flex;
   min-height: 100vh;
-  background: rebeccapurple;
+  background: linear-gradient(
+      135deg,
+      rgba(60, 69, 78, 1) 0%,
+      rgba(60, 69, 78, 0) 150%
+    ),
+    linear-gradient(180deg, rgba(34, 85, 144, 1) 0%, rgba(52, 146, 255, 1) 100%);
   color: #fff;
   margin-bottom: 1.45rem;
 `
@@ -17,10 +23,53 @@ const HeroInner = styled.div`
   flex: 1 0 auto;
 `
 
+const Aside = styled.aside`
+  display: flex;
+  flex: 0 0 35%;
+  padding-right: 32px;
+  justify-content: flex-end;
+  align-items: center;
+`
+
+const Content = styled.div`
+  flex: 1 0 auto;
+`
+
+const Title = styled.h1`
+  font-size: 28px;
+  font-weight: 500;
+  margin: 0;
+
+  small {
+    margin-top: 8px;
+    font-size: 20px;
+    color: #b6c7d9;
+    display: block;
+    font-weight: 400;
+  }
+`
+
+const StyledLogo = styled(Logo)`
+  width: 125px;
+`
+
 const Hero: React.SFC = () => (
   <HeroWrapper>
     <Container>
-      <HeroInner>Hello from Hero</HeroInner>
+      <HeroInner>
+        <Aside>
+          <StyledLogo />
+        </Aside>
+        <Content>
+          <Title>
+            Lodash
+            <small>
+              A modern JavaScript utility library delivering modularity,
+              performance & extras.
+            </small>
+          </Title>
+        </Content>
+      </HeroInner>
     </Container>
   </HeroWrapper>
 )
