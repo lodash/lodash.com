5e28cac61eebdb0294659b9b4d0af71983a24b70
Jean-Philippe Sirois <jp@veksen.net>
2019-03-18 20:12:05 -0400

    ui: wip on docs header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,36 @@
 import React from "react"
 import styled from "styled-components"
+import Logo from "../images/lodash.svg"
 
 interface HeaderProps {
   siteTitle?: string
 }
 
 const HeaderWrapper = styled.header`
+  display: flex;
+  align-items: center;
   background: #171f26;
   padding: 24px;
 `
 
+const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding-right: 20px;
+  margin-right: 20px;
+  border-right: 2px solid #293845;
+`
+
+const StyledLogo = styled(Logo)`
+  width: 58px;
+`
+
 const Header: React.SFC<HeaderProps> = ({ siteTitle }) => (
-  <HeaderWrapper>hello from header</HeaderWrapper>
+  <HeaderWrapper>
+    <LogoWrapper>
+      <StyledLogo />
+    </LogoWrapper>
+  </HeaderWrapper>
 )
 
 Header.defaultProps = {
