9ee466ad82305fc3f27207b203ed8f58f01f4c8d
Jean-Philippe Sirois <jp@veksen.net>
2020-06-19 09:51:44 -0400

    ui(docs): style `see all` button

--- src/components/DocsContent/styles.tsx
+++ src/components/DocsContent/styles.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import Button from "../Button"
+import { lighten } from "polished"
 
 export const DocsContentWrapper = styled.main`
   position: relative;
@@ -17,4 +18,12 @@ export const Content = styled.div`
 
 export const SeeAll = styled(Button)`
   margin-bottom: 24px;
+  background: #293845;
+  box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.15), 0px 6px 10px #171f26;
+  border-radius: 3px;
+
+  &:hover,
+  &:focus {
+    background: ${lighten(0.08, "#293845")};
+  }
 `
