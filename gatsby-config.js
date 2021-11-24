91b2e0a4656079cf5e19afad5d7c962131d6348d
Jean-Philippe Sirois <jp@veksen.net>
2021-11-24 09:29:37 -0500

    chore(deps): migrate project on top of fresh install of latest version of Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-create-client-paths`,
       options: { prefixes: [`/docs/*`] },
