720e09ecdf47c01823031996a4c956d9e845475f
Jean-Philippe Sirois <jp@veksen.net>
2022-11-12 09:41:33 -0500

    refact: remove dev leftover from home layout

--- src/layouts/layout-home.tsx
+++ src/layouts/layout-home.tsx
@@ -1,12 +1,7 @@
 import React from "react"
 
 const LayoutHome = (props: any) => {
-  return (
-    <>
-      home layout
-      {props.children}
-    </>
-  )
+  return <>{props.children}</>
 }
 
 export default LayoutHome
