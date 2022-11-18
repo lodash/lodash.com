ee7e3109437c7193fdb07ae83706d59151f129d9
Jean-Philippe Sirois <jp@veksen.net>
2022-11-18 08:25:42 -0500

    feat: add metadata to method and category pages

--- src/templates/method-category-page.tsx
+++ src/templates/method-category-page.tsx
@@ -1,12 +1,18 @@
 import { graphql } from "gatsby"
 import React from "react"
 import MultipleMethod from "../components/DocsContent/multiple"
+import SEO from "../components/SEO"
 import { IMethod } from "../types"
 
 const MethodCategoryPage = (props: any) => {
   const methods = props.data.allLodashMethod.edges as IMethod[]
 
-  return <MultipleMethod category={props.pageContext.categoryName} methods={methods} />
+  return (
+    <>
+      <SEO title={`${props.pageContext.categoryName} methods`} />
+      <MultipleMethod category={props.pageContext.categoryName} methods={methods} />
+    </>
+  )
 }
 
 export default MethodCategoryPage
--- src/templates/method-page.tsx
+++ src/templates/method-page.tsx
@@ -1,12 +1,18 @@
 import { graphql } from "gatsby"
 import React from "react"
 import SingleMethod from "../components/DocsContent/single"
+import SEO from "../components/SEO"
 import { IMethod } from "../types"
 
 const MethodPage = (props: any) => {
   const method = props.data.lodashMethod as IMethod["node"]
 
-  return <SingleMethod method={method} />
+  return (
+    <>
+      <SEO title={`_.${method.name} - ${method.category}`} description={method.desc} />
+      <SingleMethod method={method} />
+    </>
+  )
 }
 
 export default MethodPage
