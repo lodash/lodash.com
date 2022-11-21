ca1d0c291002b358016e88980f13c988dc51e4a4
Jean-Philippe Sirois <jp@veksen.net>
2022-11-21 12:10:58 -0500

    chore(deps): bump eslint to 8.28.0

--- src/components/Code/index.tsx
+++ src/components/Code/index.tsx
@@ -1,8 +1,18 @@
 import React from "react"
 import Highlight from "react-syntax-highlighter"
+import prettier from "prettier/standalone"
+import plugins from "prettier/parser-babel"
 import Container from "../Container"
 import lodashSyntaxHighlighting from "./lodash-syntax-highlighting"
 import * as S from "./styles"
+
+function formatByWidth(code, width) {
+  return prettier.format(code, {
+    parser: "babel",
+    printWidth: width,
+    plugins: [plugins],
+  })
+}
 
 interface ICodeProps {
   children: string
@@ -18,11 +28,23 @@ const Code = ({
 }: ICodeProps): JSX.Element => {
   const MaybeContainer = withContainer ? Container : React.Fragment
 
+  const formattedCode = React.useMemo(() => {
+    return {
+      40: formatByWidth(children, 40),
+      80: formatByWidth(children, 80),
+    }
+  }, [children])
+
+  console.log(formattedCode)
+
   return (
     <S.CodeWrapper {...restProps}>
       <MaybeContainer>
-        <Highlight language={lang} style={lodashSyntaxHighlighting}>
-          {children.trim()}
+        <Highlight className="mobile-code" language={lang} style={lodashSyntaxHighlighting}>
+          {formattedCode[40]}
+        </Highlight>
+        <Highlight className="desktop-code" language={lang} style={lodashSyntaxHighlighting}>
+          {formattedCode[80]}
         </Highlight>
       </MaybeContainer>
     </S.CodeWrapper>
--- src/components/Code/styles.tsx
+++ src/components/Code/styles.tsx
@@ -5,4 +5,16 @@ export const CodeWrapper = styled("div", {
   padding: "32px 0",
   background: "#1f2a34",
   fontSize: "16px",
+
+  ".desktop-code": {
+    "@media screen and (max-width: 1023px)": {
+      display: "none !important",
+    },
+  },
+
+  ".mobile-code": {
+    "@media screen and (min-width: 1024px)": {
+      display: "none !important",
+    },
+  },
 })
